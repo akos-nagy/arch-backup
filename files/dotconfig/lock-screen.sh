3c3312b6cc88ea7d6da97cf425a47e2823e00116
akos-nagy <akosnagymath@gmail.com>
2022-02-01 20:19:18 -0800

    feb 1

--- files/dotconfig/lock-screen.sh
+++ files/dotconfig/lock-screen.sh
@@ -1,1 +1,1 @@
-/usr/bin/swaylock --daemonize --ignore-empty-password --screenshots --effect-blur 7x5 --indicator --indicator-radius 100 --clock --timestr='%l:%M %p' --datestr='%A, %-m/%-e/%y' ;
+/usr/bin/swaylock --daemonize --ignore-empty-password --screenshots --effect-blur 7x5 --indicator --indicator-radius 100 --clock --timestr='%l:%M %p' --datestr='%A, %-m/%-e/%y' --fade-in 0.5 ;
